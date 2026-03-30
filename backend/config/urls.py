e27282254aef6621b582bef2caaa4c32baa4315c
adelrabeh <adelrabeh@gmail.com>
2026-03-30 10:28:04 +0300

    Update urls.py

--- backend/config/urls.py
+++ backend/config/urls.py
@@ -1,11 +1,15 @@
 from django.contrib import admin
 from django.urls import path
-from django.http import HttpResponse
+from django.http import JsonResponse
 
 def home(request):
-    return HttpResponse("PMO Backend Running ✅")
+    return JsonResponse({"message": "PMO Backend Running 🚀"})
+
+def test_api(request):
+    return JsonResponse({"status": "API Working ✅"})
 
 urlpatterns = [
     path("", home),
     path("admin/", admin.site.urls),
+    path("api/test/", test_api),
 ]
